d018f24ea2aa31e19453ef5102254e9d9a435503
markm <markmutisya72@gmail.com>
2025-08-04 00:48:45 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require("dotenv");
+const mongoose = require('mongoose')
+const app = express()
+const port = 3000
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("db Connected")).catch((err) => console.log(err))
+
+app.get('/', (req, res) => res.send('Furniture World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
