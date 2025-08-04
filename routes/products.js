82a473481f230127d8762df6f98fbf2968790678
markm <markmutisya72@gmail.com>
2025-08-04 01:33:10 -0700

    Updates to seaarch get and post product

---
+++ routes/products.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const productsController = require("../controllers/productsControllers");
+
+router.get('/', productsController.getAllProduct)
+router.get('/:id', productsController.getProduct)
+router.get('/search/:key', productsController.searchProduct)
+router.post('/', productsController.createProduct)
+
+module.exports = router
+
